f2fe0cdeb3c998d55e3a55b9aff35e18a2e6be51
mpk <martaprosniak@gmail.com>
2024-02-07 00:13:15 +0100

    Components placement inside game

---
+++ src/app/game/available-colors/available-colors.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'available-colors',
+  templateUrl: './available-colors.component.html',
+  styleUrl: './available-colors.component.css'
+})
+export class AvailableColorsComponent {
+
+}
