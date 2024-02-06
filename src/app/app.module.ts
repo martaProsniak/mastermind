6fd42797e60d153a9ff7dfcdd778ed117a8b2a8b
mpk <martaprosniak@gmail.com>
2024-02-07 00:13:15 +0100

    Components placement inside game

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,14 @@ import { ColorDotComponent } from './game/color-dot/color-dot.component';
 import { HintComponent } from './game/hint/hint.component';
 import { ColorCodeComponent } from './game/color-code/color-code.component';
 import { FooterComponent } from './footer/footer.component';
+import { AvailableColorsComponent } from './game/available-colors/available-colors.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    GameComponent, BoardComponent, SettingsComponent, ColorsRowComponent, ColorDotComponent, HintComponent, ColorCodeComponent, FooterComponent
+    GameComponent, BoardComponent, SettingsComponent, ColorsRowComponent, ColorDotComponent, HintComponent, ColorCodeComponent, FooterComponent, AvailableColorsComponent
   ],
   imports: [
     BrowserModule
