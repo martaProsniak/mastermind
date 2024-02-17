a72535b30630811322a4808a2c06320fba33691b
martaprosniak <martaprosniak@gmail.com>
2024-02-17 01:57:25 +0100

    Logo created

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        fredoka: ["Fredoka", 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
 
